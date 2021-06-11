ff4742275744a08928072d51b72e55f6e25f84c6
inesdev <inessouzasp@gmail.com>
2021-06-11 18:22:19 -0300

    Aula 17

--- cursos/curso-bancodedados-sql-udemy/script_aula17.sql
+++ cursos/curso-bancodedados-sql-udemy/script_aula17.sql
@@ -14,9 +14,9 @@ SELECT NOME AS CLIENTE, SEXO, EMAIL FROM CLIENTE;
 
 SELECT NOME, SEXO, EMAIL, ENDERECO FROM CLIENTE;
 
-SELECT   EMAIL, SEXO, ENDERECO, NOME FROM CLIENTE;
+SELECT EMAIL, SEXO, ENDERECO, NOME FROM CLIENTE;
 
-SELECT   EMAIL, SEXO, ENDERECO, NOME, NOW() AS DATA_HORA FROM CLIENTE;
+SELECT EMAIL, SEXO, ENDERECO, NOME, NOW() AS DATA_HORA FROM CLIENTE;
 
 -- usar este comando com * somente para fins acadêmicos, pois por mostrar todos os dados isso acarreta em trafégo de dados desnecessários pela rede:
 
